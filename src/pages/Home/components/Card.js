e58cf74e2a11bc96abce83149c4e8d13b9ee21c6
Ryan Pratama <ru.ryanpratama@gmail.com>
2022-12-31 02:38:30 +0300

    hover

--- src/pages/Home/components/Card.js
+++ src/pages/Home/components/Card.js
@@ -1,14 +1,15 @@
 import React from "react";
+import "./custom.css";
 
 const Card = ({ cardDescProps, cardIconProps, cardTitleProps }) => {
   return (
-    <div class="card w-32 xl:w-96 lg:w-64 md:w-52 sm:w-52 shadow-xl mt-2 from-accent to-white bg-gradient-to-t">
-      <div class="card-body text-left text-primary-content">
+    <div class=" card w-32 xl:w-96 lg:w-64 md:w-52 sm:w-52 shadow-xl mt-2 from-accent to-white bg-gradient-to-t">
+      <div class="card-body   text-left text-primary-content">
         <h2 class="font-bold text-center md:text-left lg:text-left text-sm md:text-xl lg:text-2xl">
           {cardTitleProps}
         </h2>
         <p class="hidden md:flex lg:flex">{cardDescProps}</p>
-        <div class="card-actions justify-center lg:justify-end">
+        <div class="card-actions justify-center md:justify-end lg:justify-end">
           <img src={cardIconProps} class="w-12 rounded-md" />
         </div>
       </div>
