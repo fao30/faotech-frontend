c0d52e29cfe592b0c331dd8f304760cc4b11a16c
Ryan Pratama <ru.ryanpratama@gmail.com>
2022-12-31 03:12:57 +0300

    font fixed

--- src/pages/Home/components/Introduction.js
+++ src/pages/Home/components/Introduction.js
@@ -11,20 +11,23 @@ const Introduction = () => {
   }, []);
   return (
     <div class="from-primary to-secondary text-primary-content -mt-[5rem] grid place-items-center items-end bg-gradient-to-b pt-20">
+      <div class="mt-20 w-11/12 md:w-4/5 lg:w-6/12 px-4">
+        <h1 class="text-2xl sm:text-4xl lg:text-4xl font-bold mb-6 text-center ">
+          Unlimited Ideas, <br />
+          Unlimited Steps
+        </h1>
+        <h1 class="text-4xl sm:text-5xl lg:text-5xl font-bold text-center">
+          Build, manage and deliver application with FaoTech
+        </h1>
+        <h1 class="mt-6 text-2xl sm:text-4xl lg:text-4xl font-bold mb-6 text-center ">
+          A one-stop shop that delivers your application across all platforms
+        </h1>
+      </div>
+
       <div class="hero">
-        <div class="hero-content mt-20 text-center">
+        <div class="hero-content text-center">
           <div>
-            <h1 class="text-3xl sm:text-4xl lg:text-4xl font-bold mb-6 ">
-              Unlimited Ideas,
-              <br />
-              Simple Steps
-            </h1>
-            <h1 class="text-4xl sm:text-5xl lg:text-5xl font-bold">
-              Build, manage and deliver
-              <br />
-              application with FaoTech
-            </h1>
-            <button class="mt-10 btn btn-warning font-bold normal-case">
+            <button class="mt-2 btn btn-warning font-bold normal-case">
               Learn More
             </button>
             <div
