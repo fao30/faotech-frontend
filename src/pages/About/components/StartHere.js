b63ad8312b2841babe3b424a39f6d3b9394d8886
Ryan Pratama <ru.ryanpratama@gmail.com>
2023-01-04 18:40:46 +0300

    better positioning

--- src/pages/About/components/StartHere.js
+++ src/pages/About/components/StartHere.js
@@ -4,12 +4,12 @@ import { Icon } from "@iconify/react";
 const StartHere = () => {
   return (
     <div class="w-full">
-      <div class="my-16 md:mx-[5rem] lg:mx-[9.5rem] mx-10 px-0 flex justify-center content-center flex-wrap items-center rounded-xl  bg-white border-2 shadow-xl ">
-        <div className="mt-8 lg:mt-3 px-2 mb-2 flex justify-center flex-col items-center">
-          <h1 class="font-[700] leading-[36px] md:leading-[50px] lg:leading-[84px] text-[#1363DF] text-[28px] md:text-[44px] pb-1 text-center">
+      <div class="my-16 md:mx-[5rem] lg:mx-[9.5rem] mx-10 px-6 flex justify-center content-center flex-wrap items-center rounded-xl  bg-white border-2 shadow-xl ">
+        <div className="mt-8 lg:mt-3 px-[2rem] md:px-2 mb-2 flex justify-center flex-col items-center">
+          <h1 class="font-[700] leading-[36px] md:leading-[50px] lg:leading-[84px] text-[#1363DF] text-[28px] md:text-[44px] pb-1 sm:text-center text-left">
             Let’s create something amazing together
           </h1>
-          <p class="px-6 text-[#757575] text-[16px] md:text-[21px] font-[400] mt-1 text-center">
+          <p class=" text-[#757575] text-[16px] md:text-[21px] font-[400] mt-1 text-left">
             Hit the button, we are there waiting for you!
           </p>
           <button class="md:w-72 w-48 mt-12 mb-8 font-[500] hover:bg-[#4C6BDC] hover:text-white normal-case text-[15px] lg:text-[20px] btn border-none relative inline-flex items-center overflow-hidden text-md text-[white] shadow-md bg-[#4C6BDC] group">
