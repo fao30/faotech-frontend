b5120f0ec17850276a821de6cd0f32173533fe05
Ryan Pratama <ru.ryanpratama@gmail.com>
2023-01-04 03:12:49 +0300

    font color adjusted

--- src/pages/Home/components/components/CardTech.js
+++ src/pages/Home/components/components/CardTech.js
@@ -6,7 +6,7 @@ const CardTech = ({ cardIconProps, cardTitleProps }) => {
       <div class="bg-white text-ourWork-100 font-bold mx-5 my-3 shadow-xl rounded-2xl">
         <div class="flex flex-row justify-start items-center w-24 h-12 sm:h-16 sm:w-40 lg:h-16 lg:w-52 ml-4">
           <div class="fixed"> {cardIconProps} </div>
-          <div class="ml-10 text-s sm:text-base md:lg:text-base font-normal">
+          <div class="text-[#4D4646] ml-10 text-s sm:text-base md:lg:text-base font-normal">
             {cardTitleProps}
           </div>
         </div>
