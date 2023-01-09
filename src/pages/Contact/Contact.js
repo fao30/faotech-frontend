ae6d18deb4d1ff94cb4e3e3fe9c6cf789ec480a7
Ryan Pratama <ru.ryanpratama@gmail.com>
2023-01-09 14:43:06 +0300

    deploy testing

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -2,7 +2,6 @@ import { useNavigate } from "react-router-dom";
 import { Icon } from "@iconify/react";
 import FormContact from "./components/FormContact";
 import { motion } from "framer-motion";
-import "../../custom.css";
 
 const Contact = () => {
   const navigate = useNavigate();
@@ -12,7 +11,7 @@ const Contact = () => {
       <div className="flex justify-end mt-10 z-20 xl:w-[60rem] xl:mx-auto ">
         {/* Button Back to Homepage */}
         <button
-          class=" font-medium hover:bg-[#4C6BDC] border-none normal-case relative inline-flex items-center px-[3.5rem] py-[0.75rem] overflow-hidden  text-[#393737] shadow-[0_2px_3px_gray] hover:text-white rounded-tr-3xl rounded-bl-3xl bg-[#FFFFFF] group"
+          class="btn hover:bg-[#4C6BDC] border-none normal-case relative inline-flex items-center px-[3.5rem] py-[0.75rem] overflow-hidden  text-[#393737] shadow-[0_2px_3px_gray] hover:text-white rounded-br-none rounded-tl-none rounded-tr-3xl rounded-bl-3xl bg-[#FFFFFF] group"
           onClick={() => navigate("/")}
         >
           <span class="absolute right-0 flex items-center justify-start  w-10 h-10 duration-300 transform translate-x-full group-hover:-translate-x-4 ease">
@@ -23,7 +22,7 @@ const Contact = () => {
               height="25"
             />
           </span>
-          <span class="relative group-hover:-translate-x-2 ease duration-300 group-hover:transform">
+          <span class="relative  group-hover:-translate-x-2 ease duration-100 group-hover:transform">
             Back to Homepage
           </span>
         </button>
@@ -43,7 +42,6 @@ const Contact = () => {
         </p>
         {/* Background  */}
         <div class="flex absolute w-6/12 right-0 lg:-right-10 sm:mr-10 h-[90%] top-20 bg-[#eaf0fa] blur-3xl -z-10 rounded-tl-full rounded-bl-full"></div>
-
         {/* FORM */}
         <FormContact />
       </div>
--- src/pages/Home/components/Header.js
+++ src/pages/Home/components/Header.js
@@ -12,24 +12,30 @@ const Header = () => {
     <div class="px-[8vw] my-[6vw] lg:my-[8vw] w-full z-10">
       <div class="w-full flex justify-between flex-wrap items-center">
         {/* HEADER TITLE */}
-        <div class="w-full md:w-[40%]">
-          <motion.div
-            initial={{ y: "-20%" }}
-            animate={{ y: "calc(100% - 100%)" }}
-          >
-            <h2 class="text-[28px] mb-2 text-primary-100 font-bold">
-              Fast, Trust, Number 1
-            </h2>
-            <h1 class="md:lg:text-5xl text-4xl font-bold text-black">
-              We Help You Develop Through Digitalization
-            </h1>
-            <h1 class="text-l my-6 text-[#757575] text-[16px] md:text-[20px]">
-              faoTech — a technology company engaged in industrial digitization
-              located in Kazan. We focus on helping you to digitalize your
-              business from the first scratch until a digital product ready to
-              use!
-            </h1>
-          </motion.div>
+
+        <motion.div
+          class="w-full md:w-[40%]"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 1 }}
+          transition={{ delay: 0.25, duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, y: -50 },
+            visible: { opacity: 1, y: 0 },
+          }}
+        >
+          <h2 class="text-[28px] mb-2 text-primary-100 font-bold">
+            Fast, Trust, Number 1
+          </h2>
+          <h1 class="md:lg:text-5xl text-4xl font-bold text-black">
+            We Help You Develop Through Digitalization
+          </h1>
+          <h1 class="text-l my-6 text-[#757575] text-[16px] md:text-[20px]">
+            faoTech — a technology company engaged in industrial digitization
+            located in Kazan. We focus on helping you to digitalize your
+            business from the first scratch until a digital product ready to
+            use!
+          </h1>
           <div class="flex justify-center">
             <button
               onClick={() => {
@@ -40,10 +46,22 @@ const Header = () => {
               Start Your Journey
             </button>
           </div>
-        </div>
+        </motion.div>
+
         {/* CAROUSEL */}
         <div class="w-full md:w-[55%] transition-all transform hover:scale-110 hover:cursor-ew-resize">
-          <div class="carousel carousel-center rounded-box space-x-4">
+          <motion.div
+            class="carousel carousel-center rounded-box space-x-4"
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 1 }}
+            transition={{ delay: 0.5, duration: 1.25 }}
+            variants={{
+              hidden: { opacity: 0, x: 50 },
+              visible: { opacity: 1, x: 0 },
+            }}
+          >
+            {" "}
             <CarouselItem
               carouselPictureProps={require("./components/screenHeader.jpeg")}
             />
@@ -53,7 +71,7 @@ const Header = () => {
             <CarouselItem
               carouselPictureProps={require("./components/screenHeader.jpeg")}
             />
-          </div>
+          </motion.div>
         </div>
       </div>
     </div>
