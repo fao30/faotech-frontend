e92ef9eed640580e79b8824f2579c19f3ff48b73
Ryan Pratama <ru.ryanpratama@gmail.com>
2023-01-12 02:32:58 +0300

    additional background added, antilaggy

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -7,7 +7,7 @@ const Contact = () => {
   const navigate = useNavigate();
 
   return (
-    <section className="flex flex-col space-y-3 pb-20 sm:pb-96 px-[8vw]">
+    <section className="flex flex-col space-y-3 pb-20 sm:pb-96 px-[8vw] from-white via-white to-blue-100 max-lg:bg-gradient-to-bl">
       <div className="flex justify-end mt-10 z-20 xl:w-[60rem] xl:mx-auto">
         {/* Button Back to Homepage */}
         <button
