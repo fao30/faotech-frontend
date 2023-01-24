4ff1edc9a50527d6159b3f270faf4c55945f8f10
Ryan Pratama <ru.ryanpratama@gmail.com>
2023-01-24 18:14:53 +0300

    hero adjusted

--- src/pages/Home/components/HeroContent.js
+++ src/pages/Home/components/HeroContent.js
@@ -21,7 +21,7 @@ const HeroContent = () => {
           </h3>
         </div>
         {/* CARDS */}
-        <div className="rounded-box w-full md:w-[55%] flex flex-wrap justify-center items-center">
+        <div className="w-full md:w-[55%] flex flex-wrap justify-center items-center">
           <motion.div
             className="flex justify-center"
             initial="hidden"
@@ -53,7 +53,7 @@ const HeroContent = () => {
             initial="hidden"
             whileInView="visible"
             viewport={{ once: true, amount: 1 }}
-            transition={{ delay: 0.4, duration: 0.4 }}
+            transition={{ delay: 0.3, duration: 0.3 }}
             variants={{
               hidden: { opacity: 0, scale: 0.5 },
               visible: { opacity: 1, scale: 1 },
@@ -61,12 +61,12 @@ const HeroContent = () => {
           >
             <CardHero
               cardDescProps={
-                "Every project includes free bug fixing up to 3 months after the handover time."
+                "Bring business operation productivity and efficiency by implementing an automation engine."
               }
-              cardTitleProps={"Bug Free Waranty"}
+              cardTitleProps={"Automation"}
               cardIconProps={
                 <Icon
-                  icon="mdi:shield-bug"
+                  icon="fluent-mdl2:connect-virtual-machine"
                   color="#4C6BDC"
                   width="62px"
                   height="67px"
