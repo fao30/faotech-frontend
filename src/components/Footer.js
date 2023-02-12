74c18a0a084c0e1b6383bac3d4394fd94e67cc6a
Ryan Pratama <ru.ryanpratama@gmail.com>
2023-02-12 03:52:57 +0300

    footer adjusted, now finally responsive

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,9 +2,25 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import { Icon } from "@iconify/react";
 import { footerOptions } from "../store/helper/NavbarOptions";
+import logo from "./assets/faoTech.png";
 
 const Footer = () => {
-  const logo = require("./assets/faoTech.png");
+  const contactData = [
+    {
+      icon: "material-symbols:mail-outline",
+      label: "partnership@faotech.dev",
+      href: "mailto:partnership@faotech.dev",
+    },
+    {
+      icon: "material-symbols:call",
+      label: "+62 877-6202-3382",
+      href: "tel:+6287762023382",
+    },
+    {
+      icon: "mdi:home-city-outline",
+      label: "Jl. Bima Citra 16 No. 2, Tambun, Bekasi, West Java, Indonesia",
+    },
+  ];
   const navigate = useNavigate();
   const scrollToTop = () => {
     window.scrollTo({
@@ -13,18 +29,19 @@ const Footer = () => {
     });
   };
   return (
-    <div className="px-[8vw] py-6 md:py-12 bg-white border-t-2 ">
-      <footer className="footer gap-y-6 flex flex-wrap justify-between items-start">
-        <div className="w-full md:w-[50%] xl:w-[60%]">
-          <label className="text-3xl font-bold   text-primary-100 shadowit flex justify-center items-center">
-            <img src={logo} className="w-7 h-7 items-center mr-1" /> faoTech.
-          </label>
+    <footer className="paddingX paddingY md:paddingX md:paddingYShorter2 border-t-2">
+      <div className="flex justify-between flex-wrap gap-y-6">
+        <div className="w-full lg:w-[50%] xl:w-[55%] 2xl:w-[60%] flex flex-col gap-y-2">
+          <div className="flex items-center">
+            <img src={logo} className="w-7 h-7 items-center mr-1" />
+            <label className="text-3xl font-bold   text-primary-100 shadowit">
+              faoTech.
+            </label>
+          </div>
           <p className="text-footer-100 pSmaller">
             © 2023. All rights reserved.
           </p>
-          {/* Slogan here */}
-          {/* <p className="text-footer-100"></p> */}
-          <div className="flex justify-start items-center ">
+          <div className="flex items-center">
             <a
               href="https://www.linkedin.com/company/faotech/"
               rel="noreferrer"
@@ -53,74 +70,51 @@ const Footer = () => {
             </a>
           </div>
         </div>
-        <div className="w-96 md:w-96 lg:w-[15%] lg:space-y-4">
-          <p>Features</p>
-          {footerOptions?.map((e) => {
-            return (
-              <p
-                onClick={() => {
-                  navigate(`${e.to}`);
-                  scrollToTop();
-                }}
-                className=" text-footer-100 link link-hover pSmaller"
-              >
-                {e.label}
-              </p>
-            );
-          })}
-        </div>
-        <div className="w-42 lg:space-y-4 items-center">
-          <p>Contact</p>
-          <div className="flex justify-center items-start">
-            <Icon
-              icon="material-symbols:mail-outline"
-              color="#757575"
-              width="24"
-              height="24"
-            />
-            <a
-              rel="noreferrer"
-              target="_blank"
-              href="mailto:partnership@faotech.dev"
-              className="link link-hover text-gray-500 ml-2 pSmaller"
-            >
-              partnership@faotech.dev
-            </a>
+        <div className="flex justify-between gap-y-6 flex-wrap w-full lg:w-[45%] xl:w-[40%] 2xl:w-[35%]">
+          <div className="flex flex-col gap-y-2 lg:gap-y-4 w-full md:w-[45%] lg:w-[30%] xl:w-[45%]">
+            <p>Features</p>
+            {footerOptions?.map((e) => {
+              return (
+                <a
+                  className="text-footer-100 cursor-pointer hover:underline"
+                  onClick={() => {
+                    navigate(`${e.to}`);
+                    scrollToTop();
+                  }}
+                >
+                  {e.label}
+                </a>
+              );
+            })}
           </div>
-          <div className="flex justify-center items-start">
-            <Icon
-              icon="material-symbols:call"
-              color="#757575"
-              width="24"
-              height="24"
-            />
-            <a
-              href="tel:+6287762023382"
-              rel="noreferrer"
-              target="_blank"
-              className="link link-hover text-gray-500 ml-2 pSmaller"
-            >
-              +62 877-6202-3382
-            </a>
-          </div>
-          <div className="flex justify-center items-start">
-            <Icon
-              icon="mdi:home-city-outline"
-              color="#757575"
-              width="24"
-              height="24"
-            />
-            <a href className="link link-hover text-gray-500 ml-2 pSmaller">
-              Jl. Bima Citra 16 No. 2,
-              <br />
-              Tambun, Bekasi,
-              <br />
-              West Java, Indonesia
-            </a>
+          <div className="flex flex-col  gap-y-2 lg:gap-y-4 w-[70%] md:w-[45%] lg:w-[55%] xl:w-[45%]">
+            <p>Contact</p>
+            {contactData?.map((e) => {
+              return (
+                <a
+                  className={`flex flex-row gap-x-2 text-footer-100 ${
+                    e?.href ? "hover:underline" : ""
+                  }`}
+                  rel="noreferrer"
+                  target="_blank"
+                  href={e?.href}
+                >
+                  <span>
+                    <Icon
+                      icon={e?.icon}
+                      color="#757575"
+                      width="24"
+                      height="24"
+                    />
+                  </span>{" "}
+                  {e?.label}
+                </a>
+              );
+            })}
           </div>
         </div>
-      </footer>
-    </div>
+      </div>
+    </footer>
   );
 };
 
